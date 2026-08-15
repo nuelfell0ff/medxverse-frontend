826d5b772b550363337db9b701b6caf58f572144
Ajayi Obaloluwa Emmanuel <obaloluwaajayi2006@gmail.com>
2026-08-15 20:34:03 +0100

    Switch API baseURL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL:  "https://benedex-backend.onrender.com/api",
-  baseURL:  "http://localhost:5000/api",
+  baseURL:  "https://benedex-backend.onrender.com/api",
+  //baseURL:  "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
